05ca5df3e7e25f6b49e7d3affa07e820fcfcd943
matia mathias <nyanzimathiaz@gmail.com>
2026-02-24 17:54:46 +0300

    adds splash screen page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ugandamartyrssacco/splashscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,9 +16,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        
-      )
+      home:Splashscreen()
     );
   }
 }
