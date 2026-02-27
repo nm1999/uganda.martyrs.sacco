24bebb2e75acf58e3fcc98784df17e361877c8eb
matia mathias <nyanzimathiaz@gmail.com>
2026-02-27 16:21:03 +0300

    adds get pacakge ans login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ugandamartyrssacco/splashscreen.dart';
+import 'package:get/get.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +15,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+  initstate(){
+    Timer.delay(Duration(seconds: 3),(){
+      Get.to(Splashscreen());
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       home:Splashscreen()
     );
   }
